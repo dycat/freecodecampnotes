bda89d4d55ece57498fa48481f9276cbc2f33fc9
dycat <8856528+dycat@users.noreply.github.com>
2022-05-15 16:26:01 +0800

    solution for convertTorome

--- JavaScript_Data_Structure_and_Algorithms/project/convertToRoman.js
+++ JavaScript_Data_Structure_and_Algorithms/project/convertToRoman.js
@@ -1,9 +1,35 @@
 function convertToRoman(num) {
-    const less_than_ten = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX']
-    if (num < 10) {
-        return less_than_ten[num - 1]
+
+    const rome = romeMap();
+    let romeNum = ''
+
+    for (const [key, value] of rome) {
+        while (num >= key){
+            romeNum += value
+            num -= key
+        }
     }
-    return num;
+    return romeNum;
+}
+
+function romeMap() {
+    let map = new Map();
+    
+    map.set(1000, 'M');
+    map.set(900, 'CM');
+    map.set(500, 'D');
+    map.set(400, 'CD');
+    map.set(100, 'C');
+    map.set(90, 'XC');
+    map.set(50, 'L');
+    map.set(40, 'XL');
+    map.set(10, 'X');
+    map.set(9, 'IX')
+    map.set(5, 'V');
+    map.set(4, "IV");
+    map.set(1, 'I');
+
+    return map;
 }
 
 module.exports = {
--- JavaScript_Data_Structure_and_Algorithms/project/convertToRoman.test.js
+++ JavaScript_Data_Structure_and_Algorithms/project/convertToRoman.test.js
@@ -2,4 +2,5 @@ const {convertToRoman} = require('./convertToRoman');
 
 test('test convert roman', () => {
     expect(convertToRoman(2)).toEqual('II');
+    expect(convertToRoman(12)).toEqual('XII')
 });

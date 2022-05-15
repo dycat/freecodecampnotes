c6bcfe3b7cac687f7636567342adea4cc02045c1
dycat <8856528+dycat@users.noreply.github.com>
2022-05-15 20:52:28 +0800

    more for regx

--- JavaScript_Data_Structure_and_Algorithms/project/telephoneCheck.js
+++ JavaScript_Data_Structure_and_Algorithms/project/telephoneCheck.js
@@ -1,5 +1,5 @@
 function telephoneCheck(str) {
-    const regex = /(\d\s)?\(?\d{3}(\)?\s?|-?)\d{3}(\s?|-?)\d{4}/g;
+    const regex = /^(1?\s?)?\(?\d{3}(\(?\s?|-?)\d{3}(\s?|-?)\d{4}$/g;
     return regex.test(str);
 }
 
